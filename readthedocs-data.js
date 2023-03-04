0f6358963193583b71efdf639290bce982d93b7e
linuxguist <122852451+linuxguist@users.noreply.github.com>
2023-03-05 01:25:00 +0530

    Add files via upload

---
+++ readthedocs-data.js
@@ -0,0 +1,22 @@
+var READTHEDOCS_DATA = {
+    project: "splash",
+    version: "stable",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://splash.readthedocs.io/en/stable/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    
+    proxied_api_host: "/_",
+    
+    commit: "6dc78f59",
+    ad_free: false,
+
+    global_analytics_code: "UA-17997319-1",
+    user_analytics_code: null
+};
+
